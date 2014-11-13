e99d5615c6843fd0eadb0e13e2bae281912ff33b
MrMihap <MrMihap@yandex.ru>
2014-11-13 17:20:40 +0300

    Классы для подключения к камере

---
+++ MEAClosedLoop/Core/CCamReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using MEAClosedLoop.Interfaces;
+
+namespace MEAClosedLoop.Core
+{
+  class CCamReader : ICamReader
+  {
+    public event OnCamPacketReadyDelegate OnCamPacketReady;
+
+    public CCamReader() { }
+
+
+  }
+}
---
+++ MEAClosedLoop/Interfaces/Delegates.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+namespace MEAClosedLoop.Interfaces
+{
+  public delegate void OnCamPacketReadyDelegate(Bitmap bmp);
+}
