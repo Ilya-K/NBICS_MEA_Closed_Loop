959ecae59002a5b67f1bfdecb8d459a29542aea9
Mikhail <mihap@fnbic.ru>
2015-09-14 10:33:41 +0300

    обновление отрисовки

--- MEAClosedLoop/Common/CPack.cs
+++ MEAClosedLoop/Common/CPack.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Diagnostics;
 namespace MEAClosedLoop
 {
   using TTime = System.UInt64;
   using TData = System.Double;
   using TFltDataPacket = Dictionary<int, System.Double[]>;
-  using TSpikesBinary =  Dictionary<int, bool[]>;
+  using TSpikesBinary = Dictionary<int, bool[]>;
   using TAbsStimIndex = System.UInt64;
-  
+
   // Pack events can be of two types:
   // S: Start, EOP = false; T: Stop, EOP = true
   // Length > 0 indicates T type of a pack event
   // In this case Start time means time of the pack end.
   // Data contains Param.PRE_SPIKE samples before the first spike and Param.POST_SPIKE samples after the EOP
   // But Start points the real start of the pack, i.e. the absolute time of the first spike in the pack
-  
+
   public class CPack
   {
     private TTime start;
@@ -29,6 +29,7 @@ namespace MEAClosedLoop
     public Int32 Length { get { return length; } set { length = Length; } }
     public TFltDataPacket Data { get { return data; } }
     public bool EOP { get { return length != 0; } }
+    public TFltDataPacket description;
 
     public CPack(TTime _start, Int32 _length, TFltDataPacket _data = null, TData[] _noiseLevel = null)
     {
@@ -36,7 +37,55 @@ namespace MEAClosedLoop
       length = _length;
       data = _data;
       noiseLevel = _noiseLevel;
+      description = null;
     }
+    /// <summary>
+    /// Заполняет внутренний масив данными описательной функции
+    /// по формуле f[x] = f[x-1] + U[x] / 30 - f[x-1]/Decrease
+    /// </summary>
+    /// <param name="windowWidth"> Длина окна сглаживания</param>
+    /// <param name="Decrease"> Обратная скорость убывания</param>
+    public void BuildDescription(int windowWidth = 140, int Decrease = 460)
+    {
+      Stopwatch w = new Stopwatch();
+      w.Start();
+      int window = windowWidth;
+      if (data.Keys.Count == 0) return;
+      double[] averages = new double[Data[Data.Keys.First()].Length];
+      foreach (int key in data.Keys)
+      //int key = 0;
+      {
+        Average stat = new Average();
+        for (int i = 0; i < 200; i++)
+        {
+          if (Data[key][i] > 0) stat.AddValueElem(Data[key][i]);
+        }
+        stat.Calc();
+        double[] fxs = new double[Data[key].Length];
+        fxs[0] = Math.Abs(Data[key][0]);
+        double last;
+
+        for (int i = 1; i < Data[key].Length; i++)
+        {
+          last = fxs[i - 1];
+          fxs[i] = (Data[key][i] > stat.Sigma * 3) ? last + Math.Abs(Data[key][i]) / 30 - last / 460 : last - last / 460;
+        }
+        
+        double average = stat.Sigma * 4;
+        for (int i = 0; i < window; i++)
+        {
+          averages[i] = average;
+        }
+        for (int i = window; i < Data[key].Length; i++)
+        {
+          average += (fxs[i] - fxs[i - window]) / (double)window;
+          averages[i] = average > 0 ? average : 0;
+        }
+      }
+      w.Stop();
+
+    }
+    
   }
   public struct SEvokedPack
   {
--- MEAClosedLoop/FBurstDescription.cs
+++ MEAClosedLoop/FBurstDescription.cs
@@ -11,72 +11,86 @@ namespace MEAClosedLoop
 {
   public partial class FBurstDescription : Form, IRecieveBusrt
   {
+    object PlotLock = new object();
     public FBurstDescription()
     {
       InitializeComponent();
     }
     void IRecieveBusrt.RecieveBurst(CPack Burst)
     {
-      GraphPane pane = PlotGraph.GraphPane;
-      pane.CurveList.Clear();
+      Burst.BuildDescription();
+      lock (PlotLock)
+        try
+        {
+          GraphPane pane = PlotGraph.GraphPane;
+          pane.CurveList.Clear();
+          //pane.Legend.IsVisible = false;
+          PointPairList f1_list = new PointPairList();
 
-      PointPairList f1_list = new PointPairList();
+          // Создадим список точек для кривой f2(x)
+          PointPairList f2_list = new PointPairList();
+          // Создадим список точек для кривой f2(x)
+          PointPairList f3_list = new PointPairList();
+          double[] Data = Burst.Data[(int)numericUpDown1.Value];
+          for (int i = 0; i < Data.Length; i++)
+          {
+            f1_list.Add(i / 25.0, Data[i]);
+          }
+          //f2_list.Add(Data[0], 0);
+          int window = 120;
+          double[] averages = new double[Data.Length];
 
-      // Создадим список точек для кривой f2(x)
-      PointPairList f2_list = new PointPairList();
-      // Создадим список точек для кривой f2(x)
-      PointPairList f3_list = new PointPairList();
-      double[] Data = Burst.Data[(int)numericUpDown1.Value];
-      for (int i = 0; i < Data.Length; i++)
-      {
-        f1_list.Add(i / 25.0, Data[i]);
-      }
-      //f2_list.Add(Data[0], 0);
-      int window = 120;
-      double[] averages = new double[Data.Length];
+          Average stat = new Average();
+          for (int i = 0; i < 200; i++)
+          {
+            if (Data[i] > 0) stat.AddValueElem(Data[i]);
+          }
+          stat.Calc();
+          double[] fxs = new double[Data.Length];
+          fxs[0] = Math.Abs(Data[0]);
+          double last;
 
-      Average stat = new Average();
-      for (int i = 0; i < 200; i++)
-      {
-        if (Data[i] > 0) stat.AddValueElem(Data[i]);
-      }
-      stat.Calc();
-      double[] fxs = new double[Data.Length];
-      fxs[0] = Math.Abs(Data[0]);
-      double last;
+          for (int i = 1; i < Data.Length; i++)
+          {
+            last = fxs[i - 1];
+            fxs[i] = (Data[i] > stat.Sigma * 3) ? last + Math.Abs(Data[i]) / 30 - last / 460 : last - last / 460;
+          }
 
-      for (int i = 1; i < Data.Length; i++)
-      {
-        last = fxs[i - 1];
-        fxs[i] = (Data[i] > stat.Sigma * 3) ? last + Math.Abs(Data[i]) / 30 - last / 460 : last - last / 460;
-      }
+          double average = stat.Sigma * 4; 
+          for (int i = 0; i < window; i++)
+          {
+            averages[i] = average;
+          }
 
-      double average = stat.Sigma * 4; // fxs.Take(window).Average() / (double)window;
-      for (int i = 0; i < window; i++)
-      {
-        averages[i] = average;
-      }
+          for (int i = window; i < Data.Length; i++)
+          {
+            average += (fxs[i] - fxs[i - window]) / (double)window;
+            averages[i] = average > 0 ? average : 0;
+          }
+          for (int i = 1; i < Data.Length; i++)
+          {
+            f3_list.Add(i / 25.0, fxs[i]);
+          }
+          for (int i = 0; i < Data.Length; i += 25)
+          {
+            f2_list.Add(i / 25.0, averages[i]);
+          }
 
-      for (int i = window; i < Data.Length; i++)
-      {
-        average += (fxs[i] - fxs[i - window]) / (double)window;
-        averages[i] = average > 0 ? average : 0;
-      }
-      for (int i = 1; i < Data.Length; i++)
-      {
-        f2_list.Add(i / 25.0, averages[i]);
-        f3_list.Add(i / 25.0, fxs[i]);
-      }
-      LineItem f2_curve = pane.AddCurve("Integral after Mooveing Avarage", f2_list, Color.Red, SymbolType.None);
-      LineItem f3_curve = pane.AddCurve("Integral", f3_list, Color.Green, SymbolType.None);
-      LineItem f1_curve = pane.AddCurve("Raw Burst", f1_list, Color.Blue, SymbolType.None);
-      // Вызываем метод AxisChange (), чтобы обновить данные об осях. 
-      // В противном случае на рисунке будет показана только часть графика, 
-      // которая умещается в интервалы по осям, установленные по умолчанию
-      PlotGraph.AxisChange();
+          LineItem f2_curve = pane.AddCurve("Integral after Mooveing Avarage", f2_list, Color.Red, SymbolType.None);
+          LineItem f3_curve = pane.AddCurve("Integral", f3_list, Color.Green, SymbolType.None);
+          LineItem f1_curve = pane.AddCurve("Raw Burst", f1_list, Color.FromArgb(100, 70,70,70), SymbolType.None);
 
-      // Обновляем график
-      PlotGraph.Invalidate();
+          // Вызываем метод AxisChange (), чтобы обновить данные об осях. 
+          // В противном случае на рисунке будет показана только часть графика, 
+          // которая умещается в интервалы по осям, установленные по умолчанию
+          PlotGraph.AxisChange();
+
+          // Обновляем график
+          PlotGraph.Invalidate();
+        }
+        catch (Exception e)
+        {
+        }
     }
 
     private void FBurstDescription_Load(object sender, EventArgs e)
